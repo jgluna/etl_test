8c4f2128c286dd5b0cce812f2b4f10727bdc4553
Juan Luna <jgluna85@gmail.com>
2026-01-27 16:54:49 -0500

    Create table script for taxi_rides.

---
+++ sql_scripts/taxi_rides_create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE taxi_rides (
+    pickup TIMESTAMP,
+    dropoff TIMESTAMP,
+    passengers INT,
+    distance DECIMAL(10,2),
+    fare DECIMAL(10,2),
+    tip DECIMAL(10,2),
+    tolls DECIMAL(10,2),
+    total DECIMAL(10,2),
+    color VARCHAR(50),
+    payment VARCHAR(50),
+    pickup_zone VARCHAR(100),
+    dropoff_zone VARCHAR(100),
+    pickup_borough VARCHAR(50),
+    dropoff_borough VARCHAR(50)
+);
