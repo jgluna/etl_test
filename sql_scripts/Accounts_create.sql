5c3e9f7457ebc8fecd171683b8a9eb8cbd4c809b
Juan Luna <jgluna85@gmail.com>
2026-01-29 15:11:14 -0500

    Create Accounts table

---
+++ sql_scripts/Accounts_create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Accounts (
+    AccountID INT,
+    DepositOption VARCHAR(50),
+    CustomerClass VARCHAR(50),
+    PrincipalBalance DECIMAL(15,2),
+    InterestRate DECIMAL(5,4),
+    InterestPaid DECIMAL(10,2)
+);
