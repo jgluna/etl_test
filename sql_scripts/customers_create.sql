ad54e2b37aec34a20b574f7531a459ce87800c18
Juan Luna <jgluna85@gmail.com>
2026-02-03 01:56:50 -0500

    Create table for customers

---
+++ sql_scripts/customers_create.sql
@@ -0,0 +1,14 @@
+CREATE TABLE customers (
+    Index INT NOT NULL,
+    Customer_Id VARCHAR(255) NOT NULL,
+    First_Name VARCHAR(255) NOT NULL,
+    Last_Name VARCHAR(255) NOT NULL,
+    Company VARCHAR(255) NOT NULL,
+    City VARCHAR(255) NOT NULL,
+    Country VARCHAR(255) NOT NULL,
+    Phone_1 VARCHAR(255),
+    Phone_2 VARCHAR(255),
+    Email VARCHAR(255) NOT NULL,
+    Subscription_Date DATE NOT NULL,
+    Website VARCHAR(255)
+);
